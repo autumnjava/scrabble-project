b4aac59a155c944c3ae2554ed22199eb7d900434
Alexander S <70532114+autumnjava@users.noreply.github.com>
2020-11-13 12:37:36 +0100

    Update Game.js
    Copyed Thomas start method which we will call later in startButtonListener later.

--- js/Game.js
+++ js/Game.js
@@ -1,9 +1,32 @@
 import Player from "./Player.js";
 export default class Game {
 
-  async start(){ 
+  async start() {
 
+    //this.createBoard();
+    //await this.tilesFromFile();
+    // console.table is a nice way
+    // to log arrays and objects
+    //console.log(this.board);
+    //onsole.table(this.tiles);
+    // create players
+    //this.players = [
+
+    //Create players according to desired amount of players, and their names
+    //maybe a for loop to create players? Smth like if player wants to create 3 players and entered their names, then for loop to create 3 players
+    //new Player(this, 'Player 1'),
+    //new Player(this, 'Player 2')
+
+    //];
+    //console.table(this.players);
+    // render the board + players
+    //this.render();
+
+    console.log('Starting the game.')
   }
 
+  startButtonListener() {
+    $('.button-start-game').click(() => alert('Here i will actually call the start() method'));
+  }
 
 }
