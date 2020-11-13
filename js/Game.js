96d8dbe9a17985413af11fe11517017d38668e7b
professorqtaku <professorqtaku@gmail.com>
2020-11-13 13:16:04 +0100

    form for for player name input created

--- js/Game.js
+++ js/Game.js
@@ -8,26 +8,28 @@ export default class Game {
 
   showFrontPage() {
     let formToFills = [
-      { label: 'Player 1', required: 'true' },
-      { label: 'Player 2', required: 'true' },
-      { label: 'Player 3', required: 'false' },
-      { label: 'Player 4', required: 'false' }
+      { label: 'Player 1', required: true },
+      { label: 'Player 2', required: true },
+      { label: 'Player 3', required: false },
+      { label: 'Player 4', required: false }
     ]
+    console.log('hej')
     let askPlayerNameFormDiv = $('<div class="form"></div>');
     let formTag = $('<form></form>');
     for (let formToFill of formToFills) {
-      let keys = Object.keys(formToFill);
       formTag.append(`
         <div>
         <label for="username">${formToFill.label}</lable>
-        <input type="text" id="playername" placeholder="username" ${(formToFill.required)}>
+        <input type="text" id="playername" placeholder="Write name here..." ${ formToFill.required ? 'required' : '' }>
         </div>
       `)
     }
     formTag.append(`<input type="submit" value="Submit the form">`);
+    askPlayerNameFormDiv.append(formTag);
 
-
-
-
+    //${ formToFill.required ? 'required' : '' }
+    $('body').append(askPlayerNameFormDiv);
   }
-}git
+
+
+}
