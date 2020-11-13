81a6a565b94747033a1a175906dd8a6be9afd1a9
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2020-11-13 13:56:01 +0100

    method get tiles from text file.

--- js/Game.js
+++ js/Game.js
@@ -47,14 +47,26 @@ export default class Game {
   }
 
 
+  async tilesFromFile() {
+    this.tiles = [];
+    // Read the tile info from file
+    (await $.get('tiles.txt'))
+      .split('\r').join('') // 
+      .split('\n').forEach(x => {
+        x = x.split(' ');
+        x[0] = x[0] === '_' ? ' ' : x[0];
+        // add tiles to this.tiles
+        while (x[2]--) {
+          this.tiles.push({ char: x[0], points: +x[1] })
+        }
+      });
+    // Shuffle in random order
+    this.tiles.sort(() => Math.random() - 0.5);
+  }
 
 
 
 
+  //git
 
-//git
-
-
-
-
-
+}
