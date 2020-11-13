ea83ea8f03712ca79c44c85e0a7068b0a051df5f
professorqtaku <professorqtaku@gmail.com>
2020-11-13 12:22:24 +0100

    added form in Game

--- js/Game.js
+++ js/Game.js
@@ -1,8 +1,34 @@
-import Player from "./Player.js";
+//import Player from "./Player.js";
 export default class Game {
 
   async start(){ 
+    // show the start page first
+    this.showFrontPage();
+  }
 
+  showFrontPage() { 
+    let formToFills = [
+      {label: 'Player 1', required: 'true'},
+      {label: 'Player 2', required: 'true'},
+      {label: 'Player 3', required: 'false'},
+      {label: 'Player 4', required: 'false'}
+    ]
+    let askPlayerNameFormDiv = $('<div class="form"></div>');
+    let formTag = $('<form></form>');
+    for (let formToFill of formToFills) { 
+      let keys = Object.keys(formToFill);
+      formTag.append(`
+        <div>
+        <label for="username">${formToFill.label}</lable>
+        <input type="text" id="playername" placeholder="username" ${(formToFill.required)}>
+        </div>
+      `)
+    }
+    formTag.append(`<input type="submit" value="Submit the form">`);
+
+
+    askPlayerNameFormDiv.append(formTag);
+    $('body').append(askPlayerNameFormDiv);
   }
 
 
