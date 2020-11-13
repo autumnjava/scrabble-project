d1493823cb859f2e450cd9ad2e71704080b2b105
TiredBirb <72065138+anna636@users.noreply.github.com>
2020-11-13 10:22:09 +0100

    Wrote method in Player class

--- js/Player.js
+++ js/Player.js
@@ -1,8 +1,32 @@
+
 export default class Player {
 
+
+  //Constructor for player class
   constructor(name, myGame) {
     this.name = name;
     this.myGame = myGame;
-    this.tiles = [...this.myGame.getTiles(), ' '];
+    this.currentTiles = [...this.myGame.getTiles(), ' '];
+    this.points = 0;
   }
+
+
+  //Method to write information about player and tiles
+  render() {
+
+    return `<div class="stand">
+      ${this.currentTiles.map((x, i) => `<div 
+          class="tile ${x.char ? '' : 'none'}"
+          data-player="${this.myGame.players.indexOf(this)}"
+          data-tile="${i}"
+        >
+        ${x.char || ''}
+        <span>${x.points || ''}</span>
+      </div>`).join('')}
+      </div>
+      <div class="pname">${this.name}</div>
+      `;
+  }
+
+
 }
