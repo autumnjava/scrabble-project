78bf960099e9a24ad22f93878bff9509541afaad
Alexander S <70532114+autumnjava@users.noreply.github.com>
2020-11-17 14:40:33 +0100

    fixed with player tiles

--- js/Game.js
+++ js/Game.js
@@ -217,9 +217,10 @@ export default class Game {
 
         //THIS PART NEEDS TO BE FIXED: 
 
-        //let player = that.players[+$(this).attr('data-player')];
-        //let tileIndex = +$(this).attr('data-tile');
-        //let tile = player.tiles[tileIndex];
+        let player = that.players[+$(this).attr('data-player')];
+        let tileIndex = +$(this).attr('data-tile');
+        console.log(player, tileIndex, player.tiles);
+        let tile = player.currentTiles[tileIndex];
 
         // we will need code that reacts
         // if you have moved a tile to a square on the board
@@ -236,7 +237,7 @@ export default class Game {
         if (pageX > left && pageX < right
           && pageY > top && pageY < bottom) {
           let newIndex = Math.floor(8 * (pageX - left) / $stand.width());
-          let pt = player.tiles;
+          let pt = player.currentTiles;
           // move around
           pt.splice(tileIndex, 1, ' ');
           pt.splice(newIndex, 0, tile);
