20805d0f4bfcd98cb27e60b372a3feb2c6402d15
TiredBirb <72065138+anna636@users.noreply.github.com>
2020-11-18 12:16:23 +0100

    Added user icons

--- js/Player.js
+++ js/Player.js
@@ -24,7 +24,11 @@ export default class Player {
         <span>${x.points || ''}</span>
       </div>`).join('')}
       </div>
-      <div class="pname">${this.name}</div>
+      
+      <div class="icon"><i class="fas fa-user fa-3x"></i></div>
+      <div class="pname">${this.name} </div>
+      
+      
       `;
   }
 
