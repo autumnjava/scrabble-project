5fd974f8911a30ad3468e30dc9d92e478975d24c
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2020-11-24 11:31:10 +0100

    added constructor and methods.

--- js/ButtonHandler/wordChecker.js
+++ js/ButtonHandler/wordChecker.js
@@ -1,3 +1,41 @@
-export default wordChecker { 
+export default WordChecker {
+
+  constructor(game) {
+    this.game = game;
+  }
+
+
+  checkWordWithSAOL() {
+
+
+    console.log(this.wordToCheck);
+
+    if (SAOLchecker.scrabbleOk(this.wordToCheck)) {
+      console.log(this.wordToCheck);
+      console.log('word was a word!');
+    }
+
+    else if (this.reverseWordToCheck(this.wordToCheck)) {
+      console.log('Word was a word but reversed')
+    }
+    else {
+      console.log('word was not a word');
+      this.currentPlayer.attemptCounter++;
+    }
+  }
+
+  reverseWordToCheck(reversedWord) {
+    reversedWord = '';
+    function reverseString(str) {
+      var newString = "";
+      for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+      }
+      return newString;
+    }
+    reverseString(reversedWord);
+    console.log(reversedWord);
+    SAOLchecker.scrabbleOk(reversedWord);
+  }
 
 }
