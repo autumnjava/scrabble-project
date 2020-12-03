418870e8e387a2018834dbef62604d0897f8871e
Alexander S <70532114+autumnjava@users.noreply.github.com>
2020-12-03 10:55:05 +0100

    Revert "player can now see his points under his name."
    This reverts commit 43775f743b1d3ed088a0a40df9f428864c2da16b.

--- js/ButtonHandler/wordChecker.js
+++ js/ButtonHandler/wordChecker.js
@@ -160,11 +160,7 @@ export default class WordChecker {
       console.log('points of tiles TW situation', this.tilePointsOfWord);
     } else {
       console.log('points of tiles (not in DW or TW situation)', this.tilePointsOfWord);
-      console.log(player.tilePoints, 'tile points of player')
     }
-
-    //MUST CHECK HERE IF WORD WAS CORRECT!
-    player.tilePoints += this.tilePointsOfWord;
   }
 
   removeTilesFromBoard(player) {
