3863b982055d3c25bb19cda2f21f9a4b95281c1b
Qiling <72065263+professorqtaku@users.noreply.github.com>
2020-12-03 15:39:01 +0100

    popup window created

--- js/EmptyTileHandler.js
+++ js/EmptyTileHandler.js
@@ -2,14 +2,31 @@ export default class EmptyTileHandler {
 
   constructor(game) {
     this.game = game;
+    this.createPopupBox();
   }
 
   //Check if empty tile is placed on board
   //in process
-  checkIfEmptyTile() {
-    let tile = this.game.board[this.game.y][this.game.x].tile;
-    if (tile.char == " ") {
+
+  checkIfEmptyTile(tile) { 
+    let isEmptyTile = this.isTileEmptyTile(tile);
+    if (isEmptyTile) { 
+      this.showPopup();
+      //this.changeTileChar(tile);
+    }
+  }
+
+  isTileEmptyTile(tile) {
+    if (tile) {
+      return tile.char === " ";
+    }
+    return false;
+  }
+
+  changeTileChar(tile){
+    if (tile.char === " ") {
       console.log('Empty tile found')
+
       let myBool = false;
       while (!myBool) {
 
@@ -25,6 +42,35 @@ export default class EmptyTileHandler {
           console.log('new tile on x and y:', tile)
         }
       }
+    }
+  }
+
+  createPopupBox() { 
+    return `
+      <div id="emptyTilePopupBox">
+        <div id="emptyTilePopupBoxContent">
+          <span class="popupClose">&times;</span>
+          <p>Var god och mata in en bokstav till den tomma brickan!</p>
+          <form id="emptyTileForm">
+            <input type="text" id="emptyTileInput" placeholder="Skriv in bokstaven här" minlength="1" maxlength="1" required}>
+            <input type="submit" value="Submit">
+          </form>
+        </div>
+      </div>
+    `
+  }
+
+  showPopup() { 
+    $('.gamePage').append(this.createPopupBox());
+    this.popupBox = $('#emptyTilePopupBox');
+    this.closeSpan = document.getElementsByClassName("popupClose")[0];
+
+
+    this.popupBox.css({ display: 'block' });
+
+    let that = this;
+    this.closeSpan.onclick = function () {
+      that.popupBox.css({ display: 'none' });
     }
   }
 
--- js/GameDragEvents.js
+++ js/GameDragEvents.js
@@ -82,7 +82,7 @@ export default {
       this.board[this.y][this.x].tile = player.currentTiles.splice(tileIndex, 1)[0];
       this.board[this.y][this.x].tile.possibleToMove = true;
       this.render();
-      this.emptyTileHandler.checkIfEmptyTile();
+      this.emptyTileHandler.checkIfEmptyTile(this.board[this.y][this.x].tile);
     }
   },
 
