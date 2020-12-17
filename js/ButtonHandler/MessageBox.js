88cef3b6f8e6f309c02733b8161370d099566bb1
Qiling <72065263+professorqtaku@users.noreply.github.com>
2020-12-17 17:45:09 +0100

    update

--- js/ButtonHandler/MessageBox.js
+++ js/ButtonHandler/MessageBox.js
@@ -1,17 +1,17 @@
-export default class MessageBox { 
+export default class MessageBox {
   constructor(message, messageEngWithCamelCase) {
     this.message = message;
     this.messageEngWithCamelCase = messageEngWithCamelCase;
     this.initial(message, messageEngWithCamelCase);
   }
 
-  initial(message, messageEngWithCamelCase) { 
+  initial(message, messageEngWithCamelCase) {
     this.id = 'messageBox' + messageEngWithCamelCase;
     $('.boxes').append(this.createBox(message));
     this.box = $('#' + this.id);
   }
 
-  createBox(message) { 
+  createBox(message) {
     return `
     <div class="messageBox" id="${this.id}">
       <p>${message}</p>
@@ -19,12 +19,12 @@ export default class MessageBox {
     `
   }
 
-  showMessage() { 
-    $('#' + this.id).css({ 'display': 'block', 'opacity': '1'});
+  showMessage() {
+    $('#' + this.id).css({ 'display': 'block', 'opacity': '1' });
   }
 
   hideMessage() {
-    $('#' + this.id).css({ 'display': 'none', 'transition': 'opacity 1s ease-out', 'opacity': '0'});
+    $('#' + this.id).css({ 'display': 'none', 'transition': 'opacity 1s ease-out', 'opacity': '0' });
   }
 
 }
